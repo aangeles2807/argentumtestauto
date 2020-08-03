a04c7349d74a394d4b8ae28de6054852eccbed5c
miguelencastillogar <miguel.en.castillo.gar@gmail.com>
2020-08-03 09:10:40 -0400

    carga de casos a Test Suite

--- Test Suites/APIs.ts
+++ Test Suites/APIs.ts
@@ -10,4 +10,310 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>aedadac1-4f22-4498-aae6-c54eaf130f98</testSuiteGuid>
+   <testCaseLink>
+      <guid>514755d8-471b-4f18-80ad-1afee2bfb0b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Autorizaciones/01 - Afiliados</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5d8f8d62-ef92-4342-85be-a58e7b73021b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Autorizaciones/07 - Afiliados MPP con PBS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>65797eb5-fcb1-4aac-b8e4-0e493feb49fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Autorizaciones/08 - Afiliados con MPP</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8df0d17e-c767-4adb-b434-fcecf0a64111</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Autorizaciones/09 - Afiliados con PBS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0502d60c-95bb-4b11-b52d-e79f2c124e8c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Autorizaciones/107 - Contrato sin cobertura inmediata</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a1a08904-f9ef-40be-8cd0-74d9c243415c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Consultas Ambulatorias/18 - Autorizar Afiliado PBS Prestador Exclusivo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ad0c2c77-9612-4c42-a887-c876f5b26aa0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Consultas Ambulatorias/26 - Autorizar con No. de Identificación activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3830f082-d788-450f-8151-350d792f54fc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Consultas Ambulatorias/27 - Autorizar con No. de Afiliado Inactivo o Cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3da0f10d-0568-4445-93a0-f292186fe8b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Consultas Ambulatorias/28 - Autorizar Afliafo MPP con No. de Identificación inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>62a277c3-5268-406c-9159-285bdbcac1f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Consultas Ambulatorias/29 - Autorizar Afliafo MPP con contrato inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c644e14a-45e1-4bdb-bf1c-f48ac964bffb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Consultas Psiquiatricas o Psicologicas/32 - Afiliados MPP con PBS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c7c603a7-bdcb-4c0a-b9a4-af64cc27225a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Consultas Psiquiatricas o Psicologicas/35 - Varias consultas el mismo dia con el mismo prestador</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>24811885-92c1-4ec0-96f4-a24487d0b6b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Consultas Psiquiatricas o Psicologicas/36 - Afiliados no Activos</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>69b61058-9da8-4968-9f60-7a3824d4cdcd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Estudios Especiales/53 - Contrato con cobertura inmediata</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c20381a9-1709-4372-8d89-39338d63123f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Estudios Especiales/54 - Autorizar con No. de Afiliado activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>24b4086c-11e1-4fa0-b48b-e6bf54c48bd2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Estudios Especiales/55 - Autorizar Afliado MPP con contrato activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d026b670-cdda-464a-a86f-18f7a8c5785f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Estudios Especiales/56 - Autorizar con No. de Identificación activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef2fddda-e21a-419a-9ab9-e6e9b4f9dd32</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Estudios Especiales/57 - Autorizar con No. de Afiliado Inactivo o Cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b26235b8-23f8-411d-930d-9ee54dcc2375</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Estudios Especiales/58 - Autorizar Afliado MPP con No. de Identificación inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3fc1540a-800c-4fb0-8316-f1b1a8640c39</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Estudios Especiales/59 - Autorizar Afliado MPP con contrato inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4d120684-c5a5-4e4b-8973-0dfeb648f3db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Laboratorios/37 - Afiliados con MPP</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4229caf0-b998-4e60-ae4e-a1a85b29af46</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Laboratorios/39 - Afiiados con cobertura en MPP y PBS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>271a1879-d0f2-4d98-b307-1c57c4f7869f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Laboratorios/44 - Autorizar Afliado MPP con contrato activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a866c442-0719-4f6c-b20f-ffca70afaee7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Laboratorios/45 - Autorizar con No. de Identificación activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>beaf21b8-2b93-4f8d-ba4a-4f4f6503bae0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Laboratorios/46 - Autorizar con No. de Afiliado Inactivo o Cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9d2ce1c1-c5e5-4fb4-80f6-7eaf43320474</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Laboratorios/47 - Autorizar Afliado MPP con No. de Identificación inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f6941d8c-071f-4d15-a07c-ccab21d94e71</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Laboratorios/48 - Autorizar Afliado MPP con contrato inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0062c1eb-cc4a-4246-bb4c-8e8347cf8460</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Odontología/100 - Solicitar autorización de consulta odontológica Afiliado PBS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fe8afd5e-29df-4b98-adec-ecbcb49858b3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Odontología/101 - Solicitar autorización de consulta odontológica MPP</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed1ed239-d2bd-4285-a9f5-c1331fe13000</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Odontología/98 - Odontología con Prestadores que usan Pharmacard</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7147d23c-d9b0-49ff-a250-789ecb01fa47</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rayos X/65 - Afiliados solo con Plan Básico activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>79de0d01-0a00-4449-945f-ddad9b4fe0f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rayos X/66 - Contrato con cobertura inmediata</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6a41048-2128-4873-80cc-52ef433205ca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rayos X/67 - Autorizar con No. de Afiliado activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>15af0186-1d1c-481d-918b-92bfd655d3c8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rayos X/68 - Autorizar Afliado MPP con contrato activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a6411b84-b978-424a-ba2b-c37c5207da0e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rayos X/69 - Autorizar con No. de Identificación activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>14022330-c217-473b-b728-1a27108806ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rayos X/70 - Autorizar con No. de Afiliado Inactivo o Cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>447b4fd5-afc7-4e81-ab89-258698670446</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rayos X/71 - Autorizar Afliado MPP con No. de Identificación inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>adaadc18-b5aa-43a5-9845-ac1dc9ca1175</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rayos X/72 - Autorizar Afliado MPP con contrato inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2fa33073-3b39-4948-b9ac-5781010b40d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Terapias Fisicas/80 - Autorizar con No. de Afiliado activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>78596230-c5a5-407f-a103-845ee0369456</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Terapias Fisicas/81 - Autorizar Afliado MPP con contrato activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>af3a5a92-3c08-43b6-8403-6bfeb91e9cd9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Terapias Fisicas/82 - Autorizar con No. de Identificación activo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a6ebd558-4643-4569-9c78-5f5968517bb6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Terapias Fisicas/83 - Autorizar con No. de Afiliado Inactivo o Cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a933ffaa-edf7-4d60-90c4-4e1c82284ebd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Terapias Fisicas/84 - Autorizar Afliado MPP con No. de Identificación inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38ae9824-5311-4e3f-b0ed-aec05fcd2533</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Terapias Fisicas/85 - Autorizar Afliado MPP con contrato inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0b3e388f-aa67-430d-b9de-cebad3ffd64d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Vacunas/86 - Autorizar vacunas con mayores de edad</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a20f51de-870b-470b-a7b5-eca8e9b60b6b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Vacunas/91 - Autorizar con No. de Afiliado activo menor de edad</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>47e0d25d-228e-4299-b61d-267f41f5b2d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Vacunas/92 - Autorizar con No. de Contrato activo con menores de edad</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>43aefc5f-6a12-4ef9-9187-f5118ace626a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Vacunas/93 - Autorizar con No. de Identificación activo con menores de edad</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>91c8242f-1603-497b-af29-8899e6278656</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Vacunas/94 - Autorizar con No. de Afiliado menor de edad inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9019ad6c-782e-4de7-8e49-7db552c25f11</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Vacunas/95 - Autorizar Afliafo MPP con No. de Identificación inactivo o cancelado</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>983d2d94-67f2-42c8-90ef-7e4b822e71ff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Vacunas/96 - Autorizar Afliafo MPP con contrato inactivo o cancelado</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
