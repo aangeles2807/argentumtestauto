cecb44dadcc369cb6f65516efe9b92ec60678ebc
Gerardo Felipe Pena Marte <ext.gpena@universal.com.do>
2020-08-08 13:10:33 -0400

    desarrollo de api anular

---
+++ Object Repository/Authorization/AutorizacionPortalAnular.rs
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description>Autorizacion Portal Anular</description>
+   <name>AutorizacionPortalAnular</name>
+   <tag></tag>
+   <elementGuidId>1a9cfd77-09d6-4fd9-a3bf-275b32b39bc9</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>7.5.5</katalonVersion>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://app-ars-autorizaciones-qa-eastus.azurewebsites.net/api/Autorizacion/Portal/Anular?codigoUsuario=${codigoUsuario}&amp;idInteraccion=${idInteraccion}&amp;codigoMotivo=${codigoMotivo}&amp;observacion=${observacion}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>c2a98c3c-100b-44a1-a975-cacf43a6c00a</id>
+      <masked>false</masked>
+      <name>codigoUsuario</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>640bb87d-6ef9-4b89-84ed-f5d34941d08b</id>
+      <masked>false</masked>
+      <name>idInteraccion</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>6f623cff-10fa-4f06-9bd1-9bb8eaee633c</id>
+      <masked>false</masked>
+      <name>codigoMotivo</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>38c9bada-7838-4978-935e-8b31865fddf0</id>
+      <masked>false</masked>
+      <name>variable</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
